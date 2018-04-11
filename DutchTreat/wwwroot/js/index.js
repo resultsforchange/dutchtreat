f1a64aa496f4b1752ccd9ee47f8f3191179aa2b5
Vernon Naidoo <results.for.change.za@gmail.com>
2018-04-11 18:39:24 +0200

    Completed modules 3 and 4. installed bower

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,24 @@
+﻿var x = 0;
+var s = "";
+console.log("hello Vernon");
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var buyButton = $("#buyButton");
+// add a listener to the event
+buyButton.on("click", function () {
+    console.log("Buying item");
+});
+
+var productInfo = $(".productProps li");
+productInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
+
+var $loginToggle = $("#loginToggle");
+var $popupForm = $(".popup-form");
+
+$loginToggle.on("click", function () {
+    $popupForm.fadeToggle(1000);
+});
